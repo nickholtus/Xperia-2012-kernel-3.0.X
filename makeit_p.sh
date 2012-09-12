a81a78b115b51ccd57c7be97c6715985c4eeb860
hayashikejinan <hayashikejinan@gmail.com>
2012-09-12 14:08:13 +0900

    fix make boot img

--- makeit_p.sh
+++ makeit_p.sh
@@ -2,7 +2,8 @@
 
 # ready zImage
 rm zImage
-rm kernel.elf
+#rm kernel.elf
+rm boot.img
 rm ramdisk.img
 cp kernel/arch/arm/boot/zImage zImage
 
@@ -12,5 +13,5 @@ find . | cpio --quiet -H newc -o | gzip > ../ramdisk.img
 cd ..
 
 # make kernel
-python mkelf.py -o kernel-unsigned.elf zImage@0x00008000 ramdisk.img@0x1000000,ramdisk
+./mkbootimg --cmdline 'cachepolicy=writealloc noinitrd init=init board_id=1 logo.nologo root=/dev/ram0 rw rootwait mem=96M@0 mem_mtrace=15M@96M mem_mshared=1M@111M mem_modem=16M@112M mem=32M@128M mem_issw=1M@160M hwmem=167M@161M mem=696M@328M vmalloc=384M console=ttyAMA2,115200n8 androidboot.console=ttyAMA2 androidboot.hardware=st-ericsson' --kernel zImage --ramdisk ramdisk.img --base 0x0 --output boot.img
 
